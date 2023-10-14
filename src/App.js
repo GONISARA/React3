2c81f697e08e3012cae01fcd9a78149b0d7a7506
gonisara <sairamgoditi@gmail.com>
2023-10-14 21:29:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,53 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import "./FetchData.css"; // Import a CSS file for styling
+import React, { useState } from "react";
+import { ChromePicker } from "react-color";
+import styled from "styled-components";
 
-function FetchData() {
-  const [records, setRecords] = useState([]);
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: ${({ selectedColor }) => selectedColor};
+`;
 
-  useEffect(() => {
-    axios
-      .get("https://dummyjson.com/users")
-      .then((res) => {
-        setRecords(res.data.users); // Access the "users" property in the response
-      })
-      .catch((err) => {
-        console.error(err); // Log the error to the console
-        setRecords([]); // Set records to an empty array or handle the error as needed
-      });
-  }, []);
+const ColorBox = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 10px;
+`;
+
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("#ffffff");
+  const [showColorPicker, setShowColorPicker] = useState(false);
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color.hex);
+  };
+
+  const handleButtonClick = () => {
+    setShowColorPicker(!showColorPicker);
+  };
 
   return (
-    <div className="center-text">
-      <h1>Dummy Data</h1>
-      <table className="styled-table">
-        <thead>
-          <tr>
-            <th>Sno</th>
-            <th>Profile Pic</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Gender</th>
-            <th>E-mail</th>
-            <th>Username</th>
-            <th>Domain</th>
-            <th>IP</th>
-            <th>University</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map((record, i) => (
-            <tr key={i}>
-              <td>{record.id}</td>
-              <td>
-                <img
-                  src={record.image}
-                  alt={`Profile Pic ${i}`}
-                  width="50"
-                  height="50"
-                />
-              </td>
-              <td>{record.firstName}</td>
-              <td>{record.lastName}</td>
-              <td>{record.gender}</td>
-              <td>{record.email}</td>
-              <td>{record.username}</td>
-              <td>{record.domain}</td>
-              <td>{record.ip}</td>
-              <td>{record.university}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <AppWrapper selectedColor={selectedColor}>
+      <ColorBox>
+        <ButtonWrapper>
+          <button onClick={handleButtonClick}>Select Color</button>
+        </ButtonWrapper>
+        {showColorPicker && (
+          <ChromePicker color={selectedColor} onChange={handleColorChange} />
+        )}
+      </ColorBox>
+    </AppWrapper>
   );
-}
+};
 
-export default FetchData;
+export default App;
